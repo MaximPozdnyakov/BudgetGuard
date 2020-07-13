0fb927ad8cb088e465c97a75bcc232abe616a8ae
MaximPozdnyakow <maximpozdnyakow@gmail.com>
2020-07-14 03:50:14 +0700

    create routes and controller for transactions

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Transaction;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return App\Transaction::all();
+    }
+
+    /**
+     * Store a newly spent resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'moneyAmount' => 'required|numeric',
+            'moneySign' => 'required|boolean',
+            'category' => 'required|string',
+            'spent_at' => 'required|date',
+        ]);
+
+        $transaction = new App\Transaction;
+        $transaction->moneyAmount = $request->moneyAmount;
+        $transaction->moneySign = $request->moneySign;
+        $transaction->category = $request->category;
+        $transaction->spent_at = $request->spent_at;
+
+        $transaction->save();
+
+        return $transaction;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return App\Transaction::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'moneyAmount' => 'required|numeric',
+            'moneySign' => 'required|boolean',
+            'category' => 'required|string',
+            'spent_at' => 'required|date',
+        ]);
+
+        $transaction = App\Transaction::findOrFail($id)->update([
+            'moneyAmount' => $request->moneyAmount,
+            'moneySign' => $request->moneySign,
+            'category' => $request->category,
+            'spent_at' => $request->spent_at
+            ]);
+
+        return $transaction;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $transaction = App\Transaction::findOrFail($id);
+        $transaction->delete();
+        return $transaction;
+    }
+}
