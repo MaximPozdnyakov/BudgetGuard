07a1ac781efcfad462ac5fe0ca9fdd390684a538
MaximPozdnyakow <maximpozdnyakow@gmail.com>
2020-07-14 03:46:07 +0700

    create transactions migrations,seeder and model

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Transaction::class, function (Faker $faker) {
+    return [
+        'moneyAmount' => $faker->randomFloat($nbMaxDecimals = 1, $min = -10000, $max = 10000),
+        'moneySign'  => $faker->randomElement($array = array(true, false)),
+        'category' => $faker->word,
+        'spent_at' => $faker->dateTime()
+    ];
+});
---
+++ database/migrations/2020_07_07_112848_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->float('moneyAmount');
+            $table->boolean('moneySign');
+            $table->string('category');
+            $table->date('spent_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
