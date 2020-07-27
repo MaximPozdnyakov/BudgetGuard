b5424856540896757289353379b42f90eaa301fd
MaximPozdnyakow <maximpozdnyakow@gmail.com>
2020-07-27 20:39:21 +0700

    v 0.9999

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -19,10 +19,10 @@
             crossorigin="anonymous"
         />
 
-        <link href="../../public/css/app.css" rel="stylesheet" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
     </head>
     <body>
         <div id="root"></div>
-        <script src="../../public/js/app.js"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
