4fa90d7fef80b4a4c4e1f16bb94d2321fc897cc8
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-21 12:16:13 +0300

    Refactor transactions reducer, add constants, create a lot of helpers functions, simplify work with dates by connecting date-fns, move SORT_TRANSACTIONS_BY_DATE and UPDATE_TRANSACTIONS_FILTERS to helper functions, delete TRANSACTION_NOT_LOADED and TRANSACTION_LOADED

--- resources/js/reducers/transactions.js
+++ resources/js/reducers/transactions.js
@@ -1,4 +1,19 @@
 import _ from "lodash";
+import { sub, startOfToday, endOfToday } from "date-fns";
+
+import {
+    SET_TRANSACTIONS,
+    ADD_TRANSACTION,
+    SET_TRANSACTION_ID,
+    UPDATE_TRANSACTION,
+    DELETE_TRANSACTION,
+    UPDATE_TRANSACTIONS_FILTERS,
+    SET_DATE_RANGE,
+    SET_CATEGORIES,
+    SET_MONEY_RANGE,
+    SET_SEARCH,
+    REMOVE_TRANSACTIONS
+} from "../constants";
 
 const initialState = {
     transactions: [],
@@ -6,161 +21,178 @@ const initialState = {
     transactionsFilters: {}
 };
 
+const sortByDate = transactions =>
+    transactions.sort((a, b) => new Date(a.spent_at) - new Date(b.spent_at));
+
+const filterTransactionsByWallet = ({ transactions, walletId }) =>
+    transactions.filter(({ wallet }) => wallet === walletId);
+
+const getMoneyRange = transactions => {
+    const moneyData = transactions.map(
+        ({ moneyAmount, moneySign }) => moneyAmount * (moneySign ? 1 : -1)
+    );
+    return [Math.min(...moneyData), Math.max(...moneyData)];
+};
+
+const getCategories = transactions =>
+    Object.keys(_.groupBy(transactions), "category");
+
+const getInitialDateRange = () => [
+    sub(startOfToday(), { weeks: 1 }),
+    endOfToday()
+];
+
+const getInitialFilters = ({ transactions, walletId }) => {
+    const filteredTransactions = filterTransactionsByWallet({
+        transactions,
+        walletId
+    });
+    return {
+        dateRange: getInitialDateRange(),
+        categories: getCategories(filteredTransactions),
+        search: "",
+        moneyRange: getMoneyRange(filteredTransactions)
+    };
+};
+
+const getUpdatedFilters = ({ transactions, walletId }) => {
+    const filteredTransactions = filterTransactionsByWallet({
+        transactions,
+        walletId
+    });
+    return {
+        categories: getCategories(filteredTransactions),
+        moneyRange: getMoneyRange(filteredTransactions)
+    };
+};
+
+const setTransactions = (state, action) => {
+    const { transactions, walletId } = action.payload;
+    return {
+        ...state,
+        transactions: sortByDate(transactions),
+        transactionsFilters: getInitialFilters({ transactions, walletId }),
+        isTransactionsLoaded: true
+    };
+};
+
+const addTransaction = (state, action) => {
+    const { newTransaction, walletId } = action.payload;
+    const transactions = sortByDate([...state.transactions, newTransaction]);
+    return {
+        ...state,
+        transactions,
+        transactionsFilters: {
+            ...state.transactionsFilters,
+            ...getUpdatedFilters({ transactions, walletId })
+        }
+    };
+};
+
+const updateTransaction = (state, action) => {
+    const { updatedTransaction, walletId } = action.payload;
+    const transactions = sortByDate(
+        state.transactions.map(transaction =>
+            transaction.id === updatedTransaction.id
+                ? updatedTransaction
+                : transaction
+        )
+    );
+    return {
+        ...state,
+        transactions,
+        transactionsFilters: {
+            ...state.transactionsFilters,
+            ...getUpdatedFilters({ transactions, walletId })
+        }
+    };
+};
+
+const deleteTransaction = (state, action) => {
+    const { id, walletId } = action.payload;
+    const transactions = state.transactions.filter(
+        transaction => transaction.id !== id
+    );
+    return {
+        ...state,
+        transactions,
+        transactionsFilters: {
+            ...state.transactionsFilters,
+            ...getUpdatedFilters({ transactions, walletId })
+        }
+    };
+};
+
+const setTransactionId = (state, action) => {
+    const { newId, temporaryId } = action.payload;
+    const transactions = state.transactions.map(transaction => {
+        if (transaction.temporaryId === temporaryId) {
+            transaction.id = newId;
+        }
+        return transaction;
+    });
+    return {
+        ...state,
+        transactions
+    };
+};
+
 export default function(state = initialState, action) {
     switch (action.type) {
-        case "GET_TRANSACTIONS":
-            const currentDate = new Date();
-
-            const money = action.payload
-                .filter(t => t.wallet === action.walletId)
-                .map(transaction => {
-                    if (!transaction.moneySign) {
-                        return -1 * transaction.moneyAmount;
-                    }
-                    return transaction.moneyAmount;
-                });
-
-            return {
-                ...state,
-                transactions: action.payload,
-                transactionsFilters: {
-                    dateRange: [
-                        new Date(
-                            currentDate.getFullYear(),
-                            currentDate.getMonth(),
-                            currentDate.getDate() - 7,
-                            0,
-                            0,
-                            0
-                        ),
-                        new Date(
-                            currentDate.getFullYear(),
-                            currentDate.getMonth(),
-                            currentDate.getDate(),
-                            23,
-                            59,
-                            59
-                        )
-                    ],
-                    categories: Object.keys(
-                        _.groupBy(
-                            action.payload.filter(
-                                t => t.wallet === action.walletId
-                            ),
-                            "category"
-                        )
-                    ),
-                    search: "",
-                    moneyRange: [Math.min(...money), Math.max(...money)]
-                }
-            };
-        case "ADD_TRANSACTION":
-            return {
-                ...state,
-                transactions: [...state.transactions, action.payload]
-            };
-        case "UPDATE_TRANSACTION":
-            return {
-                ...state,
-                transactions: state.transactions.map(transaction => {
-                    if (transaction.id === action.payload.id) {
-                        return action.payload;
-                    } else {
-                        return transaction;
-                    }
-                })
-            };
-        case "DELETE_TRANSACTION":
-            return {
-                ...state,
-                transactions: state.transactions.filter(
-                    transaction => transaction.id !== action.payload
-                )
-            };
-        case "TRANSACTION_NOT_LOADED":
-            return {
-                ...state,
-                isTransactionsLoaded: false
-            };
-        case "TRANSACTION_LOADED":
-            return {
-                ...state,
-                isTransactionsLoaded: true
-            };
-        case "SORT_TRANSACTIONS_BY_DATE":
-            return {
-                ...state,
-                transactions: state.transactions.sort(
-                    (a, b) => new Date(a.spent_at) - new Date(b.spent_at)
-                )
-            };
-        case "UPDATE_TRANSACTIONS_FILTERS":
-            const allTransactionsMoney = state.transactions
-                .filter(t => t.wallet === action.walletId)
-                .map(transaction => {
-                    if (!transaction.moneySign) {
-                        return -1 * transaction.moneyAmount;
-                    }
-                    return transaction.moneyAmount;
-                });
-
+        case SET_TRANSACTIONS:
+            return setTransactions(state, action);
+        case ADD_TRANSACTION:
+            return addTransaction(state, action);
+        case SET_TRANSACTION_ID:
+            return setTransactionId(state, action);
+        case UPDATE_TRANSACTION:
+            return updateTransaction(state, action);
+        case DELETE_TRANSACTION:
+            return deleteTransaction(state, action);
+        case UPDATE_TRANSACTIONS_FILTERS:
+            const { walletId } = action.payload;
             return {
                 ...state,
                 transactionsFilters: {
                     ...state.transactionsFilters,
-                    categories: Object.keys(
-                        _.groupBy(
-                            state.transactions.filter(
-                                t => t.wallet === action.walletId
-                            ),
-                            "category"
-                        )
-                    ),
-                    moneyRange: [
-                        Math.min(...allTransactionsMoney),
-                        Math.max(...allTransactionsMoney)
-                    ]
+                    ...getUpdatedFilters({
+                        transactions: state.transactions,
+                        walletId
+                    })
                 }
             };
-
-        case "SET_DATE_RANGE":
+        case SET_DATE_RANGE:
+            const { dateRange } = action.payload;
+            return {
+                ...state,
+                transactionsFilters: { ...state.transactionsFilters, dateRange }
+            };
+        case SET_CATEGORIES:
+            const { categories } = action.payload;
             return {
                 ...state,
                 transactionsFilters: {
                     ...state.transactionsFilters,
-                    dateRange: action.payload
+                    categories
                 }
             };
-        case "SET_CATEGORIES":
+        case SET_MONEY_RANGE:
+            const { moneyRange } = action.payload;
             return {
                 ...state,
                 transactionsFilters: {
                     ...state.transactionsFilters,
-                    categories: action.payload
+                    moneyRange
                 }
             };
-        case "SET_MONEY_RANGE":
+        case SET_SEARCH:
+            const { search } = action.payload;
             return {
                 ...state,
-                transactionsFilters: {
-                    ...state.transactionsFilters,
-                    moneyRange: action.payload
-                }
+                transactionsFilters: { ...state.transactionsFilters, search }
             };
-        case "SET_SEARCH":
-            return {
-                ...state,
-                transactionsFilters: {
-                    ...state.transactionsFilters,
-                    search: action.payload
-                }
-            };
-        case "REMOVE_TRANSACTIONS":
-            return {
-                ...state,
-                transactions: [],
-                transactionsFilters: {}
-            };
+        case REMOVE_TRANSACTIONS:
+            return { ...state, transactions: [], transactionsFilters: {} };
         default:
             return state;
     }
