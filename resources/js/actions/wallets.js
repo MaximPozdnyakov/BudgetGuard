7159bdb7142e546d5e171fb34747359c67b5adc7
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-24 13:17:16 +0300

    Refactor wallets actions, add constants, remove useless loading, move setting current wallet to reducer, remove stupid comments

--- resources/js/actions/wallets.js
+++ resources/js/actions/wallets.js
@@ -1,72 +1,35 @@
+import {
+    SET_WALLETS,
+    ADD_WALLET,
+    SET_CURRENT_WALLET,
+    REMOVE_WALLETS,
+    UPDATE_TRANSACTIONS_FILTERS,
+    SET_WALLETS_LOADED
+} from "../constants";
+
 import walletService from "../services/walletService";
 
-import { setMessage } from "./messages";
-
-// GET Wallets
-export const getWallets = () => async (dispatch, getState) => {
-    const wallets = await walletService.fetchWallets();
-    if (!wallets.isError) {
-        dispatch({
-            type: "GET_WALLETS",
-            payload: wallets
-        });
-        if (wallets.length !== 0) {
-            dispatch({
-                type: "SET_CURRENT_WALLET",
-                payload: wallets[0]
-            });
-        }
-    }
-    dispatch({
-        type: "WALLETS_LOADED"
-    });
+export const fetchWallets = () => async dispatch => {
+    const { fetchWallets } = walletService;
+    const wallets = await fetchWallets();
+    if (wallets.isError) dispatch({ type: SET_WALLETS_LOADED });
+    else dispatch({ type: SET_WALLETS, payload: { wallets } });
 };
 
-// ADD Wallet
-export const addWallet = wallet => async (dispatch, getState) => {
-    dispatch({
-        type: "WALLETS_NOT_LOADED"
-    });
-    const newWallet = await walletService.createWallet(wallet);
-    if (newWallet.isError) {
-        dispatch(setMessage(newWallet.errors.errors, "alert", true));
-    } else {
-        dispatch({
-            type: "ADD_WALLET",
-            payload: newWallet
-        });
-        dispatch({
-            type: "SET_CURRENT_WALLET",
-            payload: newWallet
-        });
-    }
-    dispatch({
-        type: "WALLETS_LOADED"
-    });
+export const addWallet = wallet => async dispatch => {
+    dispatch({ type: ADD_WALLET, payload: { wallet } });
+    const { createWallet } = walletService;
+    await createWallet(wallet);
 };
 
-export const selectWallet = wallet => async (dispatch, getState) => {
+export const selectWallet = wallet => async dispatch => {
+    dispatch({ type: SET_CURRENT_WALLET, payload: { wallet } });
     dispatch({
-        type: "WALLETS_NOT_LOADED"
-    });
-    dispatch({
-        type: "SET_CURRENT_WALLET",
-        payload: wallet
-    });
-    dispatch({
-        type: "UPDATE_TRANSACTIONS_FILTERS",
-        walletId: getState().wallets.currentWallet.id
-    });
-    dispatch({
-        type: "WALLETS_LOADED"
+        type: UPDATE_TRANSACTIONS_FILTERS,
+        payload: { walletId: wallet.id }
     });
 };
 
 export const removeWallets = () => dispatch => {
-    dispatch({
-        type: "REMOVE_WALLETS"
-    });
-    dispatch({
-        type: "WALLETS_NOT_LOADED"
-    });
+    dispatch({ type: REMOVE_WALLETS });
 };
