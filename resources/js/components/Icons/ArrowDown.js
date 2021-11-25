3d544481cd75abc38f26b0befc88a3b313229b31
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-25 18:48:31 +0300

    create ArrowDown icon

---
+++ resources/js/components/Icons/ArrowDown.js
@@ -0,0 +1,16 @@
+import React from "react";
+const ArrowDown = () => (
+    <svg
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        stroke="currentColor"
+        strokeWidth="2"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <polyline points="6 9 12 15 18 9"></polyline>
+    </svg>
+);
+export default ArrowDown;
