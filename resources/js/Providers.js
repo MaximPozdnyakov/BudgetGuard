11c0550f20407ae07dc77f1622c6ec4533a9bed2
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-25 18:42:32 +0300

    add StrictMode to Providers component

--- resources/js/Providers.js
+++ resources/js/Providers.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import store from "./store";
 
-import App from './components/App';
+import App from "./components/App";
 
 function Providers() {
     return (
-        <Provider store={store}>
-            <Router>
-                <App/>
-            </Router>
-        </Provider>
+        <React.StrictMode>
+            <Provider store={store}>
+                <Router>
+                    <App />
+                </Router>
+            </Provider>
+        </React.StrictMode>
     );
-} 
+}
 
 export default Providers;
 
-if (document.getElementById('root')) {
-    ReactDOM.render(<Providers />, document.getElementById('root'));
+if (document.getElementById("root")) {
+    ReactDOM.render(<Providers />, document.getElementById("root"));
 }
