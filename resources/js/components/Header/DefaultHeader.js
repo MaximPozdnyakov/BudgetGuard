562bd3b5662b97f916896cf3631540e47cdad7d4
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-11-25 18:46:54 +0300

    create DefaultHeader component

---
+++ resources/js/components/Header/DefaultHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function DefaultHeader() {
+    return (
+        <Navbar className="border-bottom px-5 mb-5 bg-white">
+            <div className="d-flex justify-content-center w-100">
+                <Link to="/">
+                    <Navbar.Brand className="font-weight-bold text-dark align-content-center">
+                        Budget App
+                    </Navbar.Brand>
+                </Link>
+            </div>
+        </Navbar>
+    );
+}
+
+export default DefaultHeader;
