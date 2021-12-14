1a8953cc4e55fb27e0bf975ba314826465d5a39c
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-14 17:30:03 +0300

    Create Plus icon

---
+++ resources/js/components/Icons/Plus.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Plus = () => (
+    <svg
+        viewBox="0 0 24 24"
+        width="22"
+        height="22"
+        stroke="currentColor"
+        strokeWidth="3"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="mr-2 d-none-440"
+    >
+        <circle cx="12" cy="12" r="10"></circle>
+        <line x1="12" y1="8" x2="12" y2="16"></line>
+        <line x1="8" y1="12" x2="16" y2="12"></line>
+    </svg>
+);
+
+export default Plus;
