665db522aef1894fdc7a82d107ce83d633bb5424
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-14 17:34:54 +0300

    Rename MainHeader to TransactionsHeader, fix imports, add prop types

--- resources/js/components/Transactions/TransactionsHeader.js
+++ resources/js/components/Transactions/TransactionsHeader.js
@@ -1,20 +1,17 @@
 import React from "react";
-
 import { connect } from "react-redux";
 import { setDateRange } from "../../actions/transactions";
-
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+import PropTypes from "prop-types";
 
-import Filters from "../Filters/Filters";
+import Filters from "./Filters/Filters";
+import TransactionForm from "./TransactionForm/TransactionFormOverlay";
 
-import AddTransaction from "../Operations/AddTransaction";
-
-function MainHeader(props) {
-    const { dateRange, setDateRange } = props;
+function TransactionsHeader({ dateRange, setDateRange }) {
     return (
         <>
             <div className="d-flex justify-content-around justify-content-sm-between align-items-center my-4">
-                <AddTransaction />
+                <TransactionForm />
                 <DateRangePicker
                     clearIcon={null}
                     rangeDivider=""
@@ -27,8 +24,13 @@ function MainHeader(props) {
     );
 }
 
+TransactionsHeader.propTypes = {
+    dateRange: PropTypes.arrayOf(PropTypes.instanceOf(Date)).isRequired,
+    setDateRange: PropTypes.func.isRequired
+};
+
 const mapStateToProps = state => ({
-    dateRange: state.transactions.transactionsFilters.dateRange
+    dateRange: state.transactions.filters.dateRange
 });
 
-export default connect(mapStateToProps, { setDateRange })(MainHeader);
+export default connect(mapStateToProps, { setDateRange })(TransactionsHeader);
