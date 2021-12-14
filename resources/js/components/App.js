33d94f5d6503b7a2474e389847b3b47df03929ed
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-14 15:29:35 +0300

    Fix imports in App component, refactor actions call, add 'checkDataLoaded' selector, add prop types

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,165 +1,81 @@
 import React, { useEffect } from "react";
-
-import { If, Else, Then } from "react-if";
-
 import { connect } from "react-redux";
-import { getTransactions } from "../actions/transactions";
-import { getWallets } from "../actions/wallets";
-import userActions from "../actions/users";
-
+import { Container } from "react-bootstrap";
 import { Switch, Route } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import { Container, Spinner } from "react-bootstrap";
+import { fetchTransactions } from "../actions/transactions";
+import { fetchWallets } from "../actions/wallets";
+import { fetchUser } from "../actions/users";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { checkDataLoaded } from "../selectors/selectors";
 
-import Operations from "./Operations/Operations";
+import Loader from "./Utils/Loader";
+import MessagesToast from "./Utils/MessagesToast";
+import Header from "./Header/Header";
+import GuestRoute from "./Routes/GuestRoute";
+import PrivateRoute from "./Routes/PrivateRoute";
+import TransactionsRoute from "./Routes/TransactionsRoute";
+
 import Overview from "./Overview/Overview";
-import Header from "./Navbar/Header";
-import Register from "./Auth/Register";
-import Login from "./Auth/Login";
-import RedirectIfNotAuthenticated from "./Utils/RedirectIfNotAuthenticated";
-import RedirectIfAuthenticated from "./Utils/RedirectIfAuthenticated";
-import RedirectIfWalletsNotLoaded from "./Utils/RedirectIfWalletsNotLoaded";
-import AddWallet from "./Wallet/AddWallet";
+import Operations from "./Operations/Operations";
+import LoginPage from "./Login/LoginPage";
+import WalletPage from "./Wallet/WalletPage";
+import RegisterPage from "./Register/RegisterPage";
 import Home from "./Home";
 import Page404 from "./Page404";
 
-function App(props) {
-    const {
-        messages,
-        getAuthenticatedUser,
-        getTransactions,
-        isTransactionsLoaded,
-        isUserLoaded,
-        isUserAuthenticated,
-        isWalletsLoaded,
-        getWallets
-    } = props;
-
+function App({ fetchUser, fetchTransactions, fetchWallets, isDataLoaded }) {
     useEffect(() => {
-        getAuthenticatedUser();
-    }, [localStorage.getItem("token")]);
+        (async () => {
+            await fetchUser();
+            await Promise.all([fetchWallets(), fetchTransactions()]);
+        })();
+    }, []);
 
-    useEffect(() => {
-        if (isUserAuthenticated && !isWalletsLoaded) {
-            getWallets();
-        }
-    }, [isUserAuthenticated]);
-
-    useEffect(() => {
-        if (isWalletsLoaded && !isTransactionsLoaded) {
-            getTransactions();
-        }
-    }, [isWalletsLoaded]);
-
-    useEffect(() => {
-        if (messages.type === "toast") {
-            if (messages.isError) {
-                for (let typeOfMessage in messages.messages) {
-                    messages.messages[typeOfMessage].forEach(message =>
-                        toast.error(message, {
-                            position: "bottom-left",
-                            autoClose: 5000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined
-                        })
-                    );
-                }
-            } else {
-                toast.success(messages.messages, {
-                    position: "bottom-left",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined
-                });
-            }
-        }
-    }, [messages]);
-
+    if (!isDataLoaded) return <Loader />;
     return (
-        <If
-            condition={
-                (isUserLoaded &&
-                    !isUserAuthenticated &&
-                    !isTransactionsLoaded &&
-                    !isWalletsLoaded) ||
-                (isUserLoaded &&
-                    isUserAuthenticated &&
-                    isTransactionsLoaded &&
-                    isWalletsLoaded)
-            }
-        >
-            <Then>
-                <ToastContainer
-                    position="bottom-left"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                />
-                <Header />
-                <Container className="px-0 px-sm-5 container-main">
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <RedirectIfWalletsNotLoaded
-                            exact
-                            path="/operations"
-                            component={Operations}
-                        />
-                        <RedirectIfWalletsNotLoaded
-                            exact
-                            path="/overview"
-                            component={Overview}
-                        />
-                        <RedirectIfNotAuthenticated
-                            exact
-                            path="/wallet/create"
-                            component={AddWallet}
-                        />
-                        <RedirectIfAuthenticated
-                            exact
-                            path="/register"
-                            component={Register}
-                        />
-                        <RedirectIfAuthenticated
-                            exact
-                            path="/login"
-                            component={Login}
-                        />
-                        <Route component={Page404} />
-                    </Switch>
-                </Container>
-            </Then>
-            <Else>
-                <div className="d-flex spinner-wrapper justify-content-center align-content-center">
-                    <Spinner animation="border" variant="primary" />
-                </div>
-            </Else>
-        </If>
+        <>
+            <MessagesToast />
+            <Header />
+            <Container className="px-0 px-sm-5 container-main">
+                <Switch>
+                    <GuestRoute exact path="/" component={Home} />
+                    <GuestRoute
+                        exact
+                        path="/register"
+                        component={RegisterPage}
+                    />
+                    <GuestRoute exact path="/login" component={LoginPage} />
+                    <PrivateRoute exact path="/wallet" component={WalletPage} />
+                    <TransactionsRoute
+                        exact
+                        path="/operations"
+                        component={Operations}
+                    />
+                    <TransactionsRoute
+                        exact
+                        path="/overview"
+                        component={Overview}
+                    />
+                    <Route component={Page404} />
+                </Switch>
+            </Container>
+        </>
     );
 }
-const mapStateToProps = state => ({
-    isUserLoaded: state.users.isUserLoaded,
-    isTransactionsLoaded: state.transactions.isTransactionsLoaded,
-    messages: state.messages,
-    isUserAuthenticated: state.users.isUserAuthenticated,
-    isWalletsLoaded: state.wallets.isWalletsLoaded
-});
+
+App.propTypes = {
+    fetchUser: PropTypes.func.isRequired,
+    fetchTransactions: PropTypes.func.isRequired,
+    fetchWallets: PropTypes.func.isRequired,
+    isDataLoaded: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({ isDataLoaded: checkDataLoaded(state) });
 
 export default connect(mapStateToProps, {
-    getAuthenticatedUser: userActions.getAuthenticatedUser,
-    getTransactions,
-    getWallets
+    fetchUser,
+    fetchTransactions,
+    fetchWallets
 })(App);
