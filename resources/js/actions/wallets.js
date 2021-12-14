1f46cfeecade903b29fd0f5027c34cf80984b827
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-14 16:17:15 +0300

    Make it so that the id of the selected wallet was saved in localStorage, fix contants import in wallet actions, reset transansactions filters after wallet creation

--- resources/js/actions/wallets.js
+++ resources/js/actions/wallets.js
@@ -3,10 +3,10 @@ import {
     ADD_WALLET,
     SET_CURRENT_WALLET,
     REMOVE_WALLETS,
-    UPDATE_TRANSACTIONS_FILTERS,
+    RESET_TRANSACTIONS_FILTERS,
     SET_WALLETS_LOADED,
     SET_WALLETS_NOT_LOADED
-} from "../constants";
+} from "../constants/redux";
 
 import walletService from "../services/walletService";
 
@@ -20,20 +20,31 @@ export const fetchWallets = () => async (dispatch, getState) => {
 
     const { fetchWallets } = walletService;
     const wallets = await fetchWallets();
+    const currentWallet =
+        wallets.find(({ id }) => id == localStorage.getItem("walletId")) ||
+        wallets[0] ||
+        {};
     if (wallets.isError) dispatch({ type: SET_WALLETS_LOADED });
-    else dispatch({ type: SET_WALLETS, payload: { wallets } });
+    else dispatch({ type: SET_WALLETS, payload: { wallets, currentWallet } });
 };
 
-export const createWallet = wallet => async dispatch => {
-    dispatch({ type: ADD_WALLET, payload: { wallet } });
+export const createWallet = newWallet => async dispatch => {
     const { createWallet } = walletService;
-    await createWallet(wallet);
+    const wallet = await createWallet(newWallet);
+
+    localStorage.setItem("walletId", wallet.id);
+    dispatch({ type: ADD_WALLET, payload: { wallet } });
+    dispatch({
+        type: RESET_TRANSACTIONS_FILTERS,
+        payload: { walletId: wallet.id }
+    });
 };
 
 export const selectWallet = wallet => async dispatch => {
+    localStorage.setItem("walletId", wallet.id);
     dispatch({ type: SET_CURRENT_WALLET, payload: { wallet } });
     dispatch({
-        type: UPDATE_TRANSACTIONS_FILTERS,
+        type: RESET_TRANSACTIONS_FILTERS,
         payload: { walletId: wallet.id }
     });
 };
