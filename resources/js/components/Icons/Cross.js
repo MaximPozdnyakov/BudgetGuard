c40f2c9bcb081fc82cc6e750a12426c34cbf7786
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-14 17:29:33 +0300

    Create Cross icon

---
+++ resources/js/components/Icons/Cross.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Cross = () => (
+    <svg
+        viewBox="0 0 24 24"
+        width="25"
+        height="25"
+        stroke="currentColor"
+        strokeWidth="2"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="text-black"
+    >
+        <line x1="18" y1="6" x2="6" y2="18"></line>
+        <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+);
+
+export default Cross;
