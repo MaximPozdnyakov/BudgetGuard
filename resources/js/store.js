b3aeadddc0b178a08602e0043f731a6362ac0426
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-14 15:21:21 +0300

    Fix initialState in store

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,15 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+
 import rootReducer from "./reducers";
+import { getInitialDateRange } from "./helpers/transactions";
 
 const initialState = {
+    messages: { messages: "", type: "", isError: false },
     user: { user: {}, isUserAuthenticated: false, isUserLoaded: false },
     wallets: { wallets: [], isWalletsLoaded: false, currentWallet: {} },
     transactions: {
         transactions: [],
         isTransactionsLoaded: false,
-        transactionsFilters: {}
+        filters: {
+            search: "",
+            moneyRange: [0, 0],
+            categories: [],
+            dateRange: getInitialDateRange()
+        }
     }
 };
 
