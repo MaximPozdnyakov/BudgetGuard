4f100a7bd2f468dedb0f1361a3c597efb240a2a3
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-12-15 12:32:49 +0300

    Refactor TransactionsList component, replace transactions calculations with selectTransactionsByFilters selector, replace moment package with date-fns, add prop types

--- resources/js/components/Operations/TransactionsList.js
+++ resources/js/components/Operations/TransactionsList.js
@@ -1,101 +1,53 @@
 import React from "react";
-
 import _ from "lodash";
-
-import moment from "moment";
-
+import { format } from "date-fns";
 import { ListGroup } from "react-bootstrap";
-
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import { If, Else, Then } from "react-if";
+import TransactionsGroup from "./TransactionsGroup";
 
-import TransactionsListByDate from "./TransactionsListByDate";
+import { selectTransactionsByFilters } from "../../selectors/selectors";
+import { transactionShape } from "../../constants/transactions";
 
-function TransactionsList(props) {
-    const {
-        transactions,
-        dateRange,
-        categories,
-        moneyRange,
-        search,
-        selectedWallet
-    } = props;
-
-    const filteredTransaction = transactions.filter(transaction => {
-        const spent_at = new Date(transaction.spent_at);
-
-        let money;
-        if (!transaction.moneySign) {
-            money = -1 * Number(transaction.moneyAmount);
-        } else {
-            money = Number(transaction.moneyAmount);
-        }
-
-        let description;
-        if (transaction.description) {
-            description = transaction.description;
-        } else {
-            description = "";
-        }
-
+function TransactionsList({ transactions }) {
+    if (!transactions.length) {
         return (
-            dateRange[1].getTime() - spent_at.getTime() >= 0 &&
-            dateRange[0].getTime() - spent_at.getTime() <= 0 &&
-            categories.includes(transaction.category) &&
-            money >= moneyRange[0] &&
-            money <= moneyRange[1] &&
-            description.includes(search) &&
-            transaction.wallet === selectedWallet.id
+            <ListGroup className="bg-white mt-3 text-center p-4">
+                <h4 className="mb-0 font-weight-bold">No transactions found</h4>
+            </ListGroup>
         );
-    });
+    }
 
-    const filteredTransactionWithFormattedDate = filteredTransaction.map(
-        transaction => ({
+    const transactionsByDate = _.groupBy(
+        transactions.map(transaction => ({
             ...transaction,
-            spent_at: moment(transaction.spent_at).format("L")
-        })
-    );
-
-    const transactionsGroupsByDateObject = _.groupBy(
-        filteredTransactionWithFormattedDate,
+            spent_at: format(new Date(transaction.spent_at), "MMMM dd, Y")
+        })),
         "spent_at"
     );
 
-    let transactionsGroupsByDateArray = [];
-    for (let transactionDate in transactionsGroupsByDateObject) {
-        transactionsGroupsByDateArray.unshift(
-            <TransactionsListByDate
-                transactions={transactionsGroupsByDateObject[transactionDate]}
-                key={transactionDate}
+    const transactionsGroups = [];
+    for (const [date, transactions] of Object.entries(transactionsByDate)) {
+        transactionsGroups.push(
+            <TransactionsGroup
+                date={date}
+                transactions={transactions}
+                key={date}
             />
         );
     }
     return (
-        <If condition={filteredTransaction.length === 0}>
-            <Then>
-                <ListGroup className="bg-white mt-3 text-center p-4">
-                    <h4 className="mb-0 font-weight-bold">
-                        No transactions found
-                    </h4>
-                </ListGroup>
-            </Then>
-            <Else>
-                <ListGroup className="bg-white mt-3">
-                    {transactionsGroupsByDateArray}
-                </ListGroup>
-            </Else>
-        </If>
+        <ListGroup className="bg-white mt-3">{transactionsGroups}</ListGroup>
     );
 }
 
+TransactionsList.propTypes = {
+    transactions: PropTypes.arrayOf(transactionShape).isRequired
+};
+
 const mapStateToProps = state => ({
-    transactions: state.transactions.transactions,
-    dateRange: state.transactions.transactionsFilters.dateRange,
-    categories: state.transactions.transactionsFilters.categories,
-    moneyRange: state.transactions.transactionsFilters.moneyRange,
-    search: state.transactions.transactionsFilters.search,
-    selectedWallet: state.wallets.currentWallet
+    transactions: selectTransactionsByFilters(state)
 });
 
 export default connect(mapStateToProps)(TransactionsList);
